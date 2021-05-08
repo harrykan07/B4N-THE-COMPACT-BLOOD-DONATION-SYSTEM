8cc9c3ef2880fd70a0e4b3da9eb75cd4084a4625
ned <misrapankaj12@gmail.com>
2021-05-08 18:10:35 +0530

    added map picker on search \nchanged donor match logic to match donor on certain area

--- search/admin.py
+++ search/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import SearchLogo
+from .models import RequestedRecord
+from .models import Points
 
 
 admin.site.register(SearchLogo)
+admin.site.register(RequestedRecord)
+admin.site.register(Points)
+
+
--- search/forms.py
+++ search/forms.py
@@ -1,62 +1,16 @@
 from django import forms
+from django.forms import ModelForm
+from .models import RequestedRecord
 
 
-class DonorSearch(forms.Form):
-    blood_group_s_choice = (
-        ("empty" , "Select blood group"),
-        ("a+" , "A+"),
-        ("a-" , "A-"),
-        ("b+" , "B+"),
-        ("b-" , "B-"),
-        ("o+" , "O+"),
-        ("o-" , "O-"),
-        ("ab+" , "AB+"),
-        ("ab-" , "AB-"),
-    )
-    select_blood_group = forms.ChoiceField(
-        choices=blood_group_s_choice,
-        widget=forms.Select(
-            attrs={'class':'form-control',
-            'required':'True',
-            },
-            ),
-    )
-
-    select_location = forms.CharField(
-        widget=forms.TextInput(
-            attrs={'class':'form-control',
-            'required':'True', 
-            'placeholder':'Hospital Address?..e.g..binalbagan'
-            }
-        ),
-    )
-    mobile_no = forms.CharField(
-        label="Contact Number",
-        max_length=10,
-        widget=forms.TextInput(
-            attrs={'class':'form-control',
-            'required':'True', 
-            'placeholder':'Enter your contact number'
-            }
-        ),
-    )
-    city = forms.CharField(
-        label="City",
-        max_length=10,
-        widget=forms.TextInput(
-            attrs={'class':'form-control',
-            'required':'True', 
-            'placeholder':'Enter City Name'
-            }
-        ),
-    )
-    units = forms.IntegerField(
-        label="Units of blood",
-        widget=forms.NumberInput(
-            attrs={'class':'form-control',
-            'required':'True', 
-            'placeholder':'Enter Units of blood needed'
-            }
-        ),
-    )
+class DonorSearch(ModelForm):
+    class Meta:
+        model = RequestedRecord
+        fields = '__all__'
+        widgets = {
+            'blood_group' : forms.Select(attrs={'class':'form-control', 'required':'True'}),
+            'units' : forms.NumberInput(attrs={'class':'form-control','max':'20', 'required':'True'}),
+            'contact_number' : forms.TextInput(attrs={'class':'form-control', 'required':'True'}),
+        }
+        exclude = ['status']
 
